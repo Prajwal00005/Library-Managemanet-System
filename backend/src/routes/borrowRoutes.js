e99433ac3ac41f802f60fcd512950099893dee11
Susmita Rai <susmitaraee0@gmail.com>
2025-08-25 14:35:57 +0545

    updated files

--- backend/src/routes/borrowRoutes.js
+++ backend/src/routes/borrowRoutes.js
@@ -6,5 +6,10 @@ const router = express.Router();
 router.post("/add", borrowController.processBorrowing);
 router.get("/stats", borrowController.getBorrowStatistics);
 
+router.get("/", borrowController.getAllBorrowings); // Display all
+router.get("/:id", borrowController.getBorrowingById); // Get by ID
+router.put("/:id", borrowController.updateBorrowing); // Update
+router.delete("/:id", borrowController.deleteBorrowing); // Delete
+router.put("/settle/:id", borrowController.settleBorrowing); // Settle
 
 export default router;
